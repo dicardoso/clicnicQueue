9938484bae471a708e912839654393fdd6784d31
Diogo Cardoso <32266873+dicardoso@users.noreply.github.com>
2024-09-19 18:28:29 -0300

    feat: add next endpoint (#2)

--- api/src/main/java/com/example/clicnicQueue/repository/TicketRepository.java
+++ api/src/main/java/com/example/clicnicQueue/repository/TicketRepository.java
@@ -2,8 +2,13 @@ package com.example.clicnicQueue.repository;
 
 import com.example.clicnicQueue.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Ticket findTopByServiceTypeIdOrderByIssuedAtDesc(Long serviceTypeId);
 
+    @Query("SELECT t FROM Ticket t WHERE t.status.id = :statusId ORDER BY t.serviceType.priority DESC, t.issuedAt ASC")
+    List<Ticket> findTicketsByStatusOrderByPriorityAndIssuedAt(Long statusId);
 }
